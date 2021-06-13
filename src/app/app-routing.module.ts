836fdc5b744838823a7667b02bd95452060b7699
gferrazf <85570692+gferrazf@users.noreply.github.com>
2021-06-12 23:14:17 -0300

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { SharedModule } from './shared/shared.module';
+import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,11 @@
+class Stock {
+  id: number | undefined;
+  name: string | undefined;
+  price: number | undefined;
+  minimum: number | undefined;
+  exclusiveMinimum: boolean | undefined;
+  date: string | undefined;
+  variation: number | undefined;
+}
+
+export default Stock;
